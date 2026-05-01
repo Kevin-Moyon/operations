2be3b774761fdfbb46ab4a8154e9712ef330c316
Kevin-Moyon <kevinjoelvallejo@gmail.com>
2026-04-30 20:26:59 -0500

    feat: :lipstick: Calculator Change with Atomic Design

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes as ReactRoutes, Route } from 'react-router-dom';
 import { Routes } from '../models/routes.model';
 import { AdditionPage } from '../pages/AdditionPage';
 import { HomePage } from '../pages/HomePage';
+import { CalculatorPage } from '../pages/CalculatorPage';
+
 
 export const AppRouter = () => {
     return (
@@ -11,6 +13,7 @@ export const AppRouter = () => {
                 <Route path={Routes.HOME} element={<HomePage />} />
 
                 <Route path={Routes.ADDITION} element={<AdditionPage />} />
+                <Route path={Routes.CALCULATOR} element={<CalculatorPage />} />
 
                 <Route path="*" element={<div>404 - Página no encontrada</div>} />
             </ReactRoutes>
